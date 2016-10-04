75936f227333f9e0d8a88fc00e429f7c0c51001c
glankk <tommy.bergman91@gmail.com>
2016-10-05 00:11:28 +0200

    added error checking to make.lua

--- patch/lua/make.lua
+++ patch/lua/make.lua
@@ -15,21 +15,27 @@ local code_file = fs:get(rom_info.code_ind)
 local text_hook = gru.gsc_load(rom_id .. "/text_hook.gsc")
 text_hook:shift(-rom_info.code_ram)
 text_hook:apply_be(code_file)
-fs:replace(rom_info.code_ind, code_file, true)
+fs:replace(rom_info.code_ind, code_file, fs:compressed(rom_info.code_ind))
 print("reassembling rom...")
 local patched_rom = fs:assemble_rom()
 print("building dependencies...")
-gru.gsc_load(rom_id .. "/patch/bss_patch.gsc"):apply_be(patched_rom)
-os.execute("make gz-" .. rom_id)
+local _,_,make_result = os.execute("make gz-" .. rom_id)
+if make_result ~= 0 then
+  error("failed to build gz", 0)
+end
 local gz = gru.blob_load("bin/gz/" .. rom_id .. "/gz.bin")
 local payload_rom = fs:prom_tail()
 local payload_ram = 0x80600000 - 0x60
 local payload_size = gz:size() + 0x60
-os.execute(string.format("make cleanldr && make ldr " .. 
-                         "CPPFLAGS='-DDMA_ROM=0x%08X -DDMA_RAM=0x%08X -DDMA_SIZE=0x%08X'",
-                         payload_rom, payload_ram, payload_size))
-local ldr = gru.blob_load("bin/ldr/ldr.bin")
+local _,_,make_result = os.execute(string.format("make cleanldr && make ldr " .. 
+                                                 "CPPFLAGS='-DDMA_ROM=0x%08X -DDMA_RAM=0x%08X -DDMA_SIZE=0x%08X'",
+                                                 payload_rom, payload_ram, payload_size))
+if make_result ~= 0 then
+  error("failed to build ldr", 0)
+end
 print("inserting payload...")
+gru.gsc_load(rom_id .. "/patch/bss_patch.gsc"):apply_be(patched_rom)
+local ldr = gru.blob_load("bin/ldr/ldr.bin")
 local old_ldr = patched_rom:copy(0x1000, 0x60)
 patched_rom:write(0x1000, ldr)
 patched_rom:write(payload_rom, old_ldr)
