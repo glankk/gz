ad111577230e1c054e04482cbe3d14720f132007
glankk <tommy.bergman91@gmail.com>
2016-10-07 01:16:55 +0200

    added manual rom selection for unrecognized roms in rom_table.lua

--- patch/lua/rom_table.lua
+++ patch/lua/rom_table.lua
@@ -29,3 +29,17 @@ rom_table =
                     code_ram  = 0x800116E0,
                   },
 }
+setmetatable(rom_table, {__index = function(t)
+  io.write("unrecognized rom. select an action;\n  0. quit\n")
+  local keys = {}
+  for k,v in pairs(t) do
+    keys[#keys + 1] = k
+    io.write(string.format("  %d. treat as `%s`\n", #keys, v.game .. "-" .. v.version .. "-" .. v.region))
+  end
+  local n = io.read("n")
+  local k = keys[n]
+  if k == nil then
+    error("operation aborted", 0)
+  end
+  return t[k]
+end})
