83ed70cb2e6734500ed5bc693b51d34e54f3eb12
adam <adamkiddle23@gmail.com>
2020-11-28 20:41:11 +0100

    added zu_inverseLookAtF and zu_MtxToMtxF

--- src/gz/zu.h
+++ src/gz/zu.h
@@ -148,6 +148,13 @@ void zu_set_lighting_ext(Gfx **p_gfx_p, Gfx **p_gfx_d);
 
 int zu_adjust_joystick(int v);
 
+void zu_inverseLookAtF(MtxF *mf,
+               float xEye, float yEye, float zEye,
+               float xAt, float yAt, float zAt,
+               float xUp, float yUp, float zUp);
+
+void zu_MtxToMtxF(Mtx* src, MtxF* dest);
+
 extern struct zu_scene_info zu_scene_info[];
 
 #endif
