ac5a68f60cc851cc457ddc0f2f1b0ef1d2903775
glank <glankk@users.noreply.github.com>
2023-05-08 02:39:50 +0200

    Remove obsolete struct memory_file

--- src/gz/gz.h
+++ src/gz/gz.h
@@ -83,29 +83,6 @@ enum cam_bhv
   CAMBHV_RADIAL,
 };
 
-struct memory_file
-{
-  z64_file_t            z_file;
-  _Bool                 entrance_override;
-  int32_t               next_entrance;
-  uint16_t              scene_index;
-  uint32_t              scene_flags[9];
-  qs510_t               start_icon_dd;
-  uint16_t              pause_screen;
-  int16_t               item_screen_cursor;
-  int16_t               quest_screen_cursor;
-  int16_t               equip_screen_cursor;
-  int16_t               map_screen_cursor;
-  int16_t               item_screen_x;
-  int16_t               equipment_screen_x;
-  int16_t               item_screen_y;
-  int16_t               equipment_screen_y;
-  int16_t               pause_screen_cursor;
-  int16_t               quest_screen_item;
-  int16_t               quest_screen_hilite;
-  int16_t               quest_screen_song;
-};
-
 enum movie_state
 {
   MOVIE_IDLE,
