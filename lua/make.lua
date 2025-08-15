e0a70627f9ca356b46e522af84b92b8f0f74c57b
Noah Philips <nsphilips7@gmail.com>
2025-08-15 11:33:20 -0400

    Fix for liboot ique and various attempts to make make rom work for ique

--- lua/make.lua
+++ lua/make.lua
@@ -35,14 +35,22 @@ print("inserting payload")
 local gz = gru.blob_load("bin/gz/" .. gz_version .. "/gz.bin")
 if gz:size() % 16 ~= 0 then gz:resize(gz:size() + 16 - gz:size() % 16) end
 local payload_rom = fs:prom_tail()
-local ldr_loc = gz:find(gru.blob_string("\x01\x23\x45\x67"))
-if ldr_loc == nil then error("cannot find ldr in gz image", 0) end
-local ldr_size = ldr_loc + 4
-gz:write32be(ldr_loc, payload_rom | 0x10000000)
-local ldr = gz:copy(0, ldr_size)
-local old_ldr = patched_rom:copy(0x1000, ldr_size)
-gz:write(0, old_ldr)
-patched_rom:write(0x1000, ldr)
+local payload_ram = 0x80400060 - 0x60
+local payload_size = gz:size() + 0x60
+local _,_,make_result = os.execute(string.format(make .. " clean-ldr && " ..
+                                                 make ..  " ldr" ..
+                                                 " CPPFLAGS='-DZ64_VERSION=Z64_OOTIQC" ..
+                                                 " -DDMA_ROM=0x%08X" ..
+                                                 " -DDMA_RAM=0x%08X" ..
+                                                 " -DDMA_SIZE=0x%08X'",
+                                                 payload_rom,
+                                                 payload_ram,
+                                                 payload_size))
+if make_result ~= 0 then error("failed to build ldr", 0) end
+
+print("inserting payload")
+local ldr = gru.blob_load("bin/ldr/ldr.bin")
+local old_ldr = patched_rom:copy(0x1000, 0x60)patched_rom:write(0x1000, ldr)
 patched_rom:write(payload_rom, gz)
 patched_rom:crc_update()
 
